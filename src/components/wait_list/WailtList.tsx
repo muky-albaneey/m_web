c6d4769096a543e6de93a22b22511352ef663430
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-02-10 14:23:58 +0100

    making changges

--- src/components/wait_list/WailtList.tsx
+++ src/components/wait_list/WailtList.tsx
@@ -19,24 +19,28 @@ export default function WaitList() {
         <h1 className="text-2xl md:text-[40px] font-dm-sans font-semibold mt-4 text-[#FFFFFF] md:leading-[38.6px] text-center">
           Reliable Protection, <br /> Simplified Repairs
         </h1>
-        <p className="text-xs md:text-[14px] font-dm-sans font-normal leading-[15px] md:leading-[19.2px] text-center mt-2 mx-auto max-w-lg text-white">
+        <p className="text-xs md:text-[14px] font-dm-sans font-normal leading-[15px] md:leading-[19.2px] text-center mt-2 mx-auto max-w-lg text-white" id="decript">
           We offer complete protection for your mobile devices, keeping you
           connected to the things that matter most.
         </p>
       </div>
       <div id="two">
-        <div className="mt-6 flex items-center justify-center w-full px-4" id="join_now">
-          <div className="flex w-full max-w-2xl bg-white/20 backdrop-blur-md overflow-hidden shadow-lg">
-            <input
-              type="email"
-              placeholder="Enter Email Address"
-              className="flex-1 px-3 py-2 sm:px-4 sm:py-3 text-white bg-transparent outline-none placeholder-white/60 text-xs sm:text-base"
-            />
-            <button className="bg-white text-[#004AAD] px-4 sm:px-6 py-2 sm:py-3 font-semibold text-[10px] sm:text-base">
-              Join Our Waitlist
-            </button>
-          </div>
-        </div>
+      <div className="flex w-full max-w-2xl overflow-hidden" id="join__now">
+  {/* Input Wrapper with Background */}
+  <div className="flex-1 bg-white/20 backdrop-blur-md shadow-lg" >
+    <input
+      type="email"
+      placeholder="Enter Email Address"
+      className="w-full px-3 py-2 sm:px-4 sm:py-3 text-white bg-transparent outline-none placeholder-white/60 text-xs sm:text-base"
+    />
+  </div>
+
+  {/* Button with Spacing */}
+  <button className="bg-white text-[#004AAD] px-4 sm:px-6 py-2 sm:py-3 font-semibold text-[10px] sm:text-base  ml-2">
+    Join Our Waitlist
+  </button>
+</div>
+
         <motion.div 
             initial={{ opacity: 0, y: 30 }} // Reduce initial Y value
             animate={{ opacity: 1, y: 10 }} // Ensure it aligns properly
